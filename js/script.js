790c82ec9489199ab45471f1dd756e0efe3c055b
mahdy alsayed <mahdyalsayed6@gmail.com>
2023-09-03 23:10:35 +0200

    create calculator app

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var output = document.querySelector("#clac");
+function display(num) {
+    output.value += num;
+}
+function Clear() {
+    output.value = "";
+}
+function Delete() {
+    output.value = output.value.slice(0, -1);
+}
+function calc() {
+    output.value = eval(output.value);
+}
